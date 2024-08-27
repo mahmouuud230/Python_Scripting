28cc87f08d9420f77117b1724215e49b18a85463
Mahmoud <makkad230@gmail.com>
2024-08-27 07:53:19 +0300

    variables

---
+++ 01_helloWorld.py
@@ -0,0 +1,10 @@
+x = 5
+y = "Hellow, World!"
+
+print(x)
+print(y)
+"""
+This is 
+multi line
+comment
+"""
---
+++ 02_Variables.py
@@ -0,0 +1,36 @@
+x = 4 
+x = "Sally"
+print(x)
+print("-----------------")
+
+# Force the Data Type of the varaible using the following functions
+x = str(3)
+y = int(3)
+
+print(type(x))
+print(type(y))
+print("-----------------")
+
+# Variable names are case-sensitive
+a = 5
+A = "John"
+
+print(a)
+print(A)
+print("-----------------")
+
+# Assign multiple values to multiple variables
+x, y, z = "Orange", "Banana", "Cherry"
+
+print(x)
+print(y)
+print(z)
+print("-----------------")
+
+# Assign one value to multiple variables
+x = y = z = "Orange"
+
+print(x)
+print(y)
+print(z)
+print("-----------------")
