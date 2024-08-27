604997f9f6a7d5f0cc1deafdfcda81e3e1e54802
Mahmoud <makkad230@gmail.com>
2024-08-27 08:58:50 +0300

    Variables

--- 02_Variables.py
+++ 02_Variables.py
@@ -34,3 +34,65 @@ print(x)
 print(y)
 print(z)
 print("-----------------")
+
+# Unpack a Collection // while upacking the number of variables should be equal to the number of elements in the collection
+fruits = ["apple", "banana", "cherry"]
+
+x, y, z = fruits
+
+print(x)
+print(y)
+print(z)
+print("-----------------")
+
+# Outpu Variables
+x = "Python"
+y="is"
+z = "awesome"
+
+print(x, y, z)
+print("-----------------")
+
+x = "Python"
+y="is"
+z = "awesome"
+
+print(x + y + z)
+print("-----------------")
+
+# Global Variables
+
+x = "awesome"
+
+def myfunc():
+    print("Python is", x)
+
+myfunc()
+print("-----------------")
+
+# Create a variable inside a function, with the same name as the global variable
+
+x = "awesoem"
+
+def myfunc():
+    x = "fantastic"
+    print("Python is", x)
+myfunc()
+print("Python is", x)
+print("-----------------")
+
+# The global Keyword
+
+x = "awesome"
+
+def myfunc():
+    global x
+    x = "fantastic"
+    print("Pythons is", x)
+
+myfunc()
+x = "Hello"
+
+print("Pythos is", x)
+print("-----------------")
+
