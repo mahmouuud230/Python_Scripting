51d3f30ce615a7f366d809b6a095d575ba90ced2
Mahmoud <makkad230@gmail.com>
2024-08-29 14:03:31 +0300

    Casting

---
+++ 04_Casting.py
@@ -0,0 +1,30 @@
+# Casting
+
+# Integers
+x = int(1)
+print(type(x), x)
+y = int(2.8)
+print(type(y), y)
+z = int("3")
+print(type(z), z)
+print("-----------------")
+
+# Floats
+x = float(1)
+print(type(x), x)
+y = float(2.8)
+print(type(y), y)
+z = float("3")
+print(type(z), z)
+w = float("4.2")
+print(type(w), w)
+print("-----------------")
+
+# Strings
+x = str("s1") 
+print(type(x), x)
+y = str(2) 
+print(type(y), y)
+z = str(3.0)
+print(type(z), z)
+print("-----------------")
